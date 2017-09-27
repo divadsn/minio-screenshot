45b2c47faa78c02d0231aeb960a1d51d2af0382e
David Sn <divad.nnamtdeis@gmail.com>
2017-09-27 19:28:36 +0200

    Add upload code and remove unused imports

--- screenshot.py
+++ screenshot.py
@@ -3,16 +3,12 @@
 import config
 import os
 import clipboard
-import requests
 import datetime
-import sys
-import argparse
 import upload
 import capture
 
 # Minio client dependencies
 from minio import Minio
-from minio.error import ResponseError
 
 # Libnotify, or notify2 if import fails
 try:
@@ -23,7 +19,47 @@ except ImportError:
     import notify2
     Notify = None
 
+# Helper function for notifications
+def notify_send(title, message, icon="dialog-information"):
+    if Notify is not None:
+        notice = Notify.Notification.new(title, message, icon)
+    else:
+        notice = notify2.Notification(title, message, icon)
+        notice.set_hint_string('x-canonical-append', '')
+    try:
+        notice.show()
+    except:
+        pass
+
+    print message
+
+# Check if tool to capture screen is defined and if it is installed
+if capture.cmd is None:
+    print "It seems like your platform (" + capture.platform + ") is not supported right now. [No capture tool defined]"
+    exit(1)
+elif not capture.is_installed():
+    print capture.name + " is not installed, aborting."
+    exit(1)
+
 # Set a local location to save screenshots
 directory = os.path.join(os.path.expanduser("~"), "Screenshots")
 if not os.path.exists(directory):
     os.makedirs(directory)
+
+# Make screenshot and save local
+filename = "Screenshot_" + datetime.datetime.now().strftime("%m-%d-%y_%I.%M.%S%p") + ".png"
+path = os.path.join(directory, filename)
+os.system("scrot -s " + path)
+
+# Upload to self-hosted S3 server using Minio
+client = Minio(config.account["host"], access_key=config.account["access_key"], secret_key=config.account["secret_key"], secure=config.account["secure"])
+
+# Put screenshot into bucket
+client.fput_object(config.account["bucket"], filename, path)
+
+# Build shareable link and copy it into clipboard
+url = ("https" if config.account["secure"] else "http") + "://" + config.account["host"] + "/" + config.account["bucket"] + "/" + filename
+clipboard.copy(url)
+
+# Show notification, fin.
+notify_send("Screenshot uploaded!", url)
